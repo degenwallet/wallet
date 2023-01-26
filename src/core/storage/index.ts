c4c58d1a7bc131c6c3114654df4f292829d653eb
endlessl8p <104884878+DegenTim@users.noreply.github.com>
2023-01-26 04:20:01 +0000

    Remove storageGlobal

--- src/core/storage/index.ts
+++ src/core/storage/index.ts
@@ -1,3 +1,2 @@
-export {storageGlobal} from './storage';
 export {StorageKeys} from './keys';
 export {reduxStorage} from './redux_storage';
--- src/core/storage/storage.ts
+++
@@ -1,7 +0,0 @@
-import {MMKV} from 'react-native-mmkv';
-
-export const storageGlobal = new MMKV({
-  id: 'global',
-  // TODO: Use value from keychain in the future. Since it's a shared storage, not critical for now
-  encryptionKey: 'degen_password',
-});
